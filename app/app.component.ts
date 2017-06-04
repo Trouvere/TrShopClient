831d191b30c245f38364dcad20649eafa4db6333
Aliaksei Mishyn <spat89@mail.ru>
2017-06-04 16:18:02 +0300

    Добавлено приложение с сервисом
    который берёт пользователей и с сервера и с JSON файла.

---
+++ app/app.component.ts
@@ -0,0 +1,46 @@
+﻿import { Component, OnInit} from '@angular/core';
+import { Response} from '@angular/http';
+import { HttpService} from './http.service';
+import {User} from './user';
+
+@Component({
+    selector: 'my-app',
+    template: `<ul>
+        <li *ngFor="let user of users">
+        <p>Имя пользователя: {{user.name}}</p>
+        <p>Возраст пользователя: {{user.age}}</p>
+        </li>
+        </ul>`,
+        
+        providers: [HttpService]
+})
+export class AppComponent implements OnInit { 
+    users1 =  
+        [   {
+               "name": "Bob",
+               "age": 28
+           },
+           {
+               "name": "Tom",
+               "age": 45
+           },
+           {
+               "name": "Alice",
+               "age": 32
+           }
+       ];
+    
+    users: User[]=[];
+//    users1: User[]=[];
+constructor(private httpService: HttpService){}
+ngOnInit(){
+    
+this.httpService.getData()
+    .subscribe((data: Response) => { this.users=data.json();
+        console.log(data);    };
+//this.httpService.getData()
+//.subscribe((data: Response) => console.log(data) );
+//  console.log(data);
+}
+
+}
---
+++ app/http.service.ts
@@ -0,0 +1,13 @@
+import {Injectable} from '@angular/core';
+import {Http} from '@angular/http';
+ 
+@Injectable()
+export class HttpService{
+ 
+    constructor(private http: Http){ }
+     
+    getData(){
+//        return this.http.get('users.json')
+        return this.http.get('http://localhost:9000/metanit/m')        
+    }
+}
